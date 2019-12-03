5471b3f83c6f9ed8c5866c5e6bbf1df05a386d26
Sung Jun <kick23@naver.com>
2019-12-03 13:14:19 +0900

    OPF-1446 web에서 fep log sequence 보기 적용

--- aimir-fep/src/main/java/com/aimir/fep/logger/SoapOutActionInInterceptor.java
+++ aimir-fep/src/main/java/com/aimir/fep/logger/SoapOutActionInInterceptor.java
@@ -1,4 +1,8 @@
 package com.aimir.fep.logger;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -6,6 +10,7 @@ import org.apache.cxf.binding.soap.SoapMessage;
 import org.apache.cxf.binding.soap.interceptor.AbstractSoapInterceptor;
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.Phase;
 
 import com.aimir.fep.logger.snowflake.SnowflakeGeneration;
@@ -15,7 +20,8 @@ public class SoapOutActionInInterceptor extends AbstractSoapInterceptor  {
 	private static Log log = LogFactory.getLog(SoapOutActionInInterceptor.class);
 	
 	public SoapOutActionInInterceptor() {
-		super(Phase.USER_PROTOCOL);
+		//super(Phase.USER_PROTOCOL);
+		super(Phase.POST_LOGICAL);
 		//addBefore(ReadHeadersInterceptor.class.getName());
 		addAfter(LoggingOutInterceptor.class.getName());
 	}
@@ -23,6 +29,17 @@ public class SoapOutActionInInterceptor extends AbstractSoapInterceptor  {
 	@Override
 	public void handleMessage(SoapMessage message) throws Fault {
 		// TODO Auto-generated method stub
+        Map<String, List> headers = (Map<String, List>) message.get(Message.PROTOCOL_HEADERS);
+        
+        try {
+        	if(headers == null) {
+        		headers = new HashMap<String, List>();
+        	}
+        	headers.put("Sequence", Collections.singletonList(SnowflakeGeneration.getId()));
+            message.put(Message.PROTOCOL_HEADERS, headers);
+        } catch (Exception ce) {
+            throw new Fault(ce);
+        }
 		SnowflakeGeneration.deleteId();
 	}
 
