5dddfec4804e2f1147b68485c41363129d4326df
sjhan <sjhan@nuritelecom.com>
2020-11-12 09:55:22 +0900

    OPF-1530 셋업 파일 수정

--- 04_SetProperties_web.sh
+++ 04_SetProperties_web.sh
@@ -1,4 +1,6 @@
 echo 'SetProperties files.....'
 cp ./properties/command.properties ./aimir-web/target/aimir-web-3.4-SNAPSHOT/WEB-INF/classes
+cp ./properties/command.properties ./aimir-web/src/main/resources/command.properties
 cp ./properties/jdbc.properties ./aimir-web/target/aimir-web-3.4-SNAPSHOT/WEB-INF/classes
+cp ./properties/jdbc.properties ./aimir-web/src/main/resources/jdbc.properties
 echo 'done.'
