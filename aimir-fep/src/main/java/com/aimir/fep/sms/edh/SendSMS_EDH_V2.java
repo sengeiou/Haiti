9b28d175010502c5a9011a4e329be4ff4f5c06be
hans <marsr0913@gmail.com>
2021-03-22 16:54:07 +0900

    OPF-2574
    클래스 생성 후 로그 확인

--- aimir-fep/src/main/java/com/aimir/fep/sms/edh/SendSMS_EDH_V2.java
+++ aimir-fep/src/main/java/com/aimir/fep/sms/edh/SendSMS_EDH_V2.java
@@ -1,12 +1,18 @@
 package com.aimir.fep.sms.edh;
 
+import javax.annotation.Resource;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
 
+import com.aimir.dao.system.SmsServiceLogDao;
 import com.aimir.fep.sms.edh.atompark.ConnectAPI;
-import com.aimir.fep.util.FMPProperty;
+import com.aimir.model.system.SmsServiceLog;
+import com.aimir.util.TimeUtil;
 
 /**
  * @since 2021.03.18
@@ -16,7 +22,13 @@ import com.aimir.fep.util.FMPProperty;
 @Service
 public class SendSMS_EDH_V2 {
 	private static Log log = LogFactory.getLog(SendSMS_EDH_V2.class);
+
+    @Resource(name="transactionManager")
+    private HibernateTransactionManager txManager;
 	
+    @Autowired
+    private SmsServiceLogDao smsServiceLogDao; 
+    
 	private SMSInterface instance;
 	
 	private String url;
@@ -43,8 +55,42 @@ public class SendSMS_EDH_V2 {
 	
 	public void send(String mobileNumber, String message) {
 		init();
+		
+		if(instance != null) {
+			String result = instance.send(mobileNumber, message);
+			if(result != null) {
+				log.debug("mobileNumber : " + mobileNumber +",sms result : " + result);
+				saveSMSLog(mobileNumber, message, result);
+			}
+		}
+		
+		log.info("send smsLog complete");
 	}
 
+	private void saveSMSLog(String mobileNumber, String message, String result) {
+		TransactionStatus txStatus = null;
+		
+		try {
+			txStatus = txManager.getTransaction(null);
+			
+			SmsServiceLog smsLog = new SmsServiceLog();
+			smsLog.setMsg(message);
+			smsLog.setReceiveNo(mobileNumber);
+			smsLog.setSendTime(TimeUtil.getCurrentTime());
+			smsLog.setResult(result);
+			
+			smsServiceLogDao.add(smsLog);
+			
+			txManager.commit(txStatus);
+		}catch(Exception e) {
+			log.error(e,e);
+			if (txStatus != null) 
+				txManager.rollback(txStatus);
+		}
+		
+		log.info("saved smsLog complete");
+	}
+	
 	private String getMobileNumber(String m) {
 		if(m == null) 
 			return null;
