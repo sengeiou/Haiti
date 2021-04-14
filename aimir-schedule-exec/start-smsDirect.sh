a5d8842cca691877eeee2809c7608ed79ca42b8a
jhdang <jhdang@nuritelecom.com>
2021-04-14 19:08:14 +0900

    OPF-2580 스크립트 수정

--- aimir-schedule-exec/start-smsDirect.sh
+++ aimir-schedule-exec/start-smsDirect.sh
@@ -7,4 +7,4 @@ do
         kill -9 $pid
 done
 
-mvn -e -f /home/aimir/aimiramm.dev/aimir-schedule-exec/pom-HaitiSMSTask.xml -DmdevId= -DsmsType=DIRECT antrun:run 2>&1 > /dev/null &
+mvn -e -f /home/aimir/aimiramm/aimir-schedule-exec/pom-HaitiSMSTask.xml -DmdevId= -DsmsType=DIRECT antrun:run 2>&1 > /dev/null &
